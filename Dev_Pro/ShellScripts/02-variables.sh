c577a320e5089f52e07eb793ec5b59399a43ff9b
KIG <KIG@Outlook.com>
2020-01-26 20:36:50 +0530

    file created

--- Dev_Pro/ShellScripts/02-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This the second script we are executig
-
-NAME=Jhon
-
-echo Hi $NAME
-echo Good Morning $NAME
-echo Good Night $NAME
--- Dev_Pro/ShellScripts/03-command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#DATE=2018-11-01
-DATE=$(date +%F)
-echo Hi, welcome today date is $DATE
